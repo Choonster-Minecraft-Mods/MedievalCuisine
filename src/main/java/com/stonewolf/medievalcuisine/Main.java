cfbc9de1948640fca1d5f5509705436e3fb13ea6
Choonster <Choonster.2010@gmail.com>
2015-12-20 00:13:26 +1100

    Fix @Instance annotation

--- src/main/java/com/stonewolf/medievalcuisine/Main.java
+++ src/main/java/com/stonewolf/medievalcuisine/Main.java
@@ -15,8 +15,8 @@ public class Main {
     public static final String MODNAME = "Medieval Cuisine";
     public static final String VERSION = "1.0.0";
         
-    @Instance
-    public static Main instance = new Main();
+    @Instance(MODID)
+    public static Main instance;
        
     @SidedProxy(clientSide="com.stonewolf.medievalcuisine.ClientProxy", serverSide="com.stonewolf.medievalcuisine.ServerProxy")
     public static CommonProxy proxy;
